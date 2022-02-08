dc82a1b992078b3d8a2d3daba69dcbb9ecad48c2
Thomas <thomas.meche@gmail.com>
2022-02-08 16:18:34 -0700

    submission Bite 189 @ codechalleng.es

---
+++ 189/control_flow.py
@@ -0,0 +1,17 @@
+import regex as re
+
+IGNORE_CHAR = 'b'
+QUIT_CHAR = 'q'
+MAX_NAMES = 5
+
+
+def filter_names(names):
+    _digits = re.compile(r'\d')
+    filtered_names = [name for name in names if name[0] != IGNORE_CHAR and _digits.search(name) is None]
+    new_names = []
+    for name in filtered_names:
+        if name[0] == QUIT_CHAR:
+            break
+        new_names.append(name)
+    return new_names[0:MAX_NAMES]
+
