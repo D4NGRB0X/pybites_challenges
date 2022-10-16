4f5407dbc61752d9bfdc59ff8ec73bc5c13c4cca
Thomas <thomas.meche@gmail.com>
2022-10-16 11:33:56 -0600

    submission Bite 95 @ codechalleng.es

---
+++ 95/bdaydict.py
@@ -0,0 +1,27 @@
+MSG = 'Hey {}, there are more people with your birthday!'
+BIRTHDAYS = []
+
+
+def check_shared_birthday(birthday):
+    shared_birthday = False
+    birthday_month_day = (birthday.month, birthday.day)
+
+    if birthday_month_day in BIRTHDAYS:
+        shared_birthday = True;
+
+    BIRTHDAYS.append(birthday_month_day)
+
+    return shared_birthday
+
+
+class BirthdayDict(dict):
+    """Override dict to print a message every time a new person is added that has
+       the same birthday (day+month) as somebody already in the dict"""
+
+    def __setitem__(self, name, birthday):
+        super().__setitem__(name, birthday)
+        if check_shared_birthday(birthday):
+            print(MSG.format(name))
+
+
+
