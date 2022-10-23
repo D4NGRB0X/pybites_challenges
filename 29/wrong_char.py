684da3ed33bf073254c4329aa351f288a67e534c
thomas <thomas@dangerbox.dev>
2022-10-23 17:40:26 -0600

    submission Bite 29 @ codechalleng.es

---
+++ 29/wrong_char.py
@@ -0,0 +1,11 @@
+def get_index_different_char(chars):
+    alpha_num_char = []
+    not_alpha_num_char = []
+    for char in chars:
+        if str(char).isalnum():
+            alpha_num_char.append(char)
+        else:
+            not_alpha_num_char.append(char)
+    if len(alpha_num_char) < len(not_alpha_num_char):
+        return chars.index(alpha_num_char[0])
+    return chars.index(not_alpha_num_char[0])
